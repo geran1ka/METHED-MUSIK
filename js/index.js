0593ec0bff3fd2a1af74715800fa2a3b040b58f9
geran1ka <roman.khoruzhy@gmail.com>
2022-11-04 23:58:21 +0300

    Урок 4. Рендер карточек, кнопки следующий и предыдущий

--- js/index.js
+++ js/index.js
@@ -1,8 +1,110 @@
+const dataMusic = [
+    {
+        id: '1',
+        artist: 'The weeknd',
+        track: 'Save your tears',
+        poster: 'img/photo1.jpg',
+        mp3: 'audio/The Weeknd - Save Your Tears.mp3',
+    },
+    {
+        id: '2',
+        artist: 'Imagine Dragons',
+        track: 'Follow You',
+        poster: 'img/photo2.jpg',
+        mp3: 'audio/Imagine Dragons - Follow You.mp3',
+    },
+    {
+        id: '3',
+        artist: 'Tove Lo',
+        track: 'How Long',
+        poster: 'img/photo3.jpg',
+        mp3: 'audio/Tove Lo - How Long.mp3',
+    },
+    {
+        id: '4',
+        artist: 'Tom Odell',
+        track: 'Another Love',
+        poster: 'img/photo4.jpg',
+        mp3: 'audio/Tom Odell - Another Love.mp3',
+    },
+    {
+        id: '5',
+        artist: 'Lana Del Rey',
+        track: 'Born To Die',
+        poster: 'img/photo5.jpg',
+        mp3: 'audio/Lana Del Rey - Born To Die.mp3',
+    },
+    {
+        id: '6',
+        artist: 'Adele',
+        track: 'Hello',
+        poster: 'img/photo6.jpg',
+        mp3: 'audio/Adele - Hello.mp3',
+    },
+    {
+        id: '7',
+        artist: 'Tom Odell',
+        track: "Can't Pretend",
+        poster: 'img/photo7.jpg',
+        mp3: "audio/Tom Odell - Can't Pretend.mp3",
+    },
+    {
+        id: '8',
+        artist: 'Lana Del Rey',
+        track: 'Young And Beautiful',
+        poster: 'img/photo8.jpg',
+        mp3: 'audio/Lana Del Rey - Young And Beautiful.mp3',
+    },
+    {
+        id: '9',
+        artist: 'Adele',
+        track: 'Someone Like You',
+        poster: 'img/photo9.jpg',
+        mp3: 'audio/Adele - Someone Like You.mp3',
+    },
+    {
+        id: '10',
+        artist: 'Imagine Dragons',
+        track: 'Natural',
+        poster: 'img/photo10.jpg',
+        mp3: 'audio/Imagine Dragons - Natural.mp3',
+    },
+    
+    {
+        id: '11',
+        artist: 'Drake',
+        track: 'Laugh Now Cry Later',
+        poster: 'img/photo11.jpg',
+        mp3: 'audio/Drake - Laugh Now Cry Later.mp3',
+    },
+    {
+        id: '12',
+        artist: 'Madonna',
+        track: 'Frozen',
+        poster: 'img/photo12.jpg',
+        mp3: 'audio/Madonna - Frozen.mp3',
+    }
+];
+
 const audio = new Audio();
 const tracksCard = document.getElementsByClassName('track');
+const catalogContainer = document.querySelector('.catalog__container');
 const player = document.querySelector('.player');
 const pauseBtn = document.querySelector('.player__controller-pause');
-const stopBtn = document.querySelector('.player__controller-stop')
+const stopBtn = document.querySelector('.player__controller-stop');
+const prevBtn = document.querySelector('.player__controller-prev');
+const nextBtn = document.querySelector('.player__controller-next');
+const likeBtn = document.querySelector('.player__controller-like');
+const muteBtn = document.querySelector('.player__controller-mute');
+
+const catalogAddBtn = document.createElement('button');
+catalogAddBtn.classList.add('catalog__btn-add');
+catalogAddBtn.innerHTML = `
+    <span>Увидеть все</span>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.59 16.59L13.17 12L8.59 7.41L10 6L16 12L10 18L8.59 16.59Z"/>
+    </svg>
+`;
 
 const pausePlayer = () => {
     const trackActive = document.querySelector('.track_active')
@@ -19,36 +121,118 @@ const pausePlayer = () => {
 }
 
 const playMusic = (event) => {
+    event.preventDefault();
     const trackActive = event.currentTarget;
-    
+
     if(trackActive.classList.contains('track_active')) {
         pausePlayer();
         return
-    } else {}
-    audio.src = trackActive.dataset.track;
+    };
+    let i = 0;
+    const id = trackActive.dataset.idTrack;
+    const track = dataMusic.find((item, index) => {
+        i = index;    
+        return id === item.id;
+    });
+
+    audio.src = track.mp3;
     audio.play();
     pauseBtn.classList.remove('player__icon_play');
     player.classList.add('player_active');
 
-    for (let i = 0; i < tracksCard.length; i++) {
-        tracksCard[i].classList.remove('track_active');
-    }
+    const prevTrack = i === 0 ? dataMusic.length - 1 : i - 1;
+    const nextTrack = i + 1 === dataMusic.length ? 0 : i + 1;
+    prevBtn.dataset.idTrack = dataMusic[prevTrack].id;
+    nextBtn.dataset.idTrack = dataMusic[nextTrack].id;
 
-    trackActive.classList.add('track_active');
+    for (let i = 0; i < tracksCard.length; i++) {
+        if (id === tracksCard[i].dataset.idTrack) {
+            tracksCard[i].classList.add('track_active');  
+        } else {
+            tracksCard[i].classList.remove('track_active');
+        }
+        
+    }
 };
 
-for (let i = 0; i < tracksCard.length; i++) {
-    tracksCard[i].addEventListener('click', playMusic);
-}
+const addHandlerTrack = () => {
+    for (let i = 0; i < tracksCard.length; i++) {
+        tracksCard[i].addEventListener('click', playMusic);
+    }
+};
+
+
 pauseBtn.addEventListener('click', pausePlayer);
 
 stopBtn.addEventListener('click', () => {
-    audio.pause();
     audio.src = '';
-    console.log('audio.src: ', audio.src);
     player.classList.remove('player_active');
+    document.querySelector('.track_active').classList.remove('track_active');
+
+    /*
     for (let i = 0; i < tracksCard.length; i++) {
         tracksCard[i].classList.remove('track_active');
-    };
-})
+    };*/
+});
+
+const createCard = (data) => {
+    const card = document.createElement('a');
+    card.href = '#';
+    card.classList.add('catalog__item', 'track');
+    /*Второй сопоб добавления класса
+    a.className = 'catalog__item track';
+    */
+    card.dataset.idTrack = data.id;
+
+    card.innerHTML = `
+        <div class="track__img-wrap">
+            <img class="track__poster" src="${data.poster}" alt="${data.artist} ${data.track}" width="180px" height="180px">
+        </div>
+
+        <div class="track__info track-info">
+            <p class="track-info__title">${data.track}</p>
+            <p class="track-info__artist">${data.artist}</p>   
+        </div>
+    `;
+
+    return card
+};
+
+const renderCatalog = (dataList) => {
+    catalogContainer.textContent = '';
+    const listCards = dataList.map(createCard);
+    catalogContainer.append(...listCards);
+    addHandlerTrack();
+}
+
+const checkCount = (i = 1) => {
+    tracksCard[0];
+    if (catalogContainer.clientHeight > tracksCard[0].clientHeight * 3) {
+        tracksCard[tracksCard.length - i].style.display = 'none';
+        checkCount(i + 1);
+    } else if (i !== 1) {
+        catalogContainer.append(catalogAddBtn);
+    }
+}
+
+const updateTime = () => {
+    console.log('updateTime');
+}
+
+const init = () => {
+    renderCatalog(dataMusic);
+    checkCount()
+
+    catalogAddBtn.addEventListener('click', () => {
+        [...tracksCard].forEach((trackCard) => {
+            trackCard.style.display = '';
+            catalogAddBtn.remove();
+        });
+    });
+    prevBtn.addEventListener('click', playMusic);
+    nextBtn.addEventListener('click', playMusic);
+    audio.addEventListener('timeupdate', updateTime);
+};
+
+init();
 
